14e9afd4930745bd16fa3e27f9a658c20913d5a7
sugandv21 <sdvsuganya@gmail.com>
2025-09-18 13:13:07 +0530

    Update Footer.jsx

--- Ecom_Frontend/src/components/Footer.jsx
+++ Ecom_Frontend/src/components/Footer.jsx
@@ -5,12 +5,10 @@ export default function Footer() {
     <footer className="bg-gradient-to-r from-purple-500 via-pink-500 to-pink-600 py-6 mt-12 shadow-inner">
       <div className="container mx-auto text-center text-white text-lg md:text-base">
         <p>
-          © {new Date().getFullYear()} MyShop. All rights reserved.
-        </p>
-        <p className="mt-1 text-white/80 text-md">
-          Designed with ❤️ for a seamless shopping experience
+          © {new Date().getFullYear()} DV Shop. All rights reserved.
         </p>
       </div>
     </footer>
   );
 }
+
